19814d843c9db058a5e06bd98b982622260951c7
Zachary  Jordan <Zachjordan0198@gmail.com>
2016-05-21 12:59:56 -0500

    Shadows

---
+++ Code Day/Assets/PlayerControllers.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllers : MonoBehaviour {
+
+	public float movementSpeed;
+
+	public GameObject playerOne;
+	public GameObject playerTwo;
+	public GameObject playerThree;
+	public GameObject playerFour;
+
+	private Vector2 playerOnePos;
+	private Vector2 playerTwoPos;
+	private Vector2 playerThreePos;
+	private Vector2 playerFourPos;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
