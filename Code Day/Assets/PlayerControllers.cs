cd8e9c64bf0ba8e92c3e5198ec89b6c44eb38db2
Zachary  Jordan <Zachjordan0198@gmail.com>
2016-05-21 13:39:54 -0500

    I love Jesse

--- Code Day/Assets/PlayerControllers.cs
+++ Code Day/Assets/PlayerControllers.cs
@@ -18,12 +18,37 @@ public class PlayerControllers : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		
+		playerOnePos = GetComponent<Transform>().position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		movementControl();
+	}
+
+	void movementControl()
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			playerOnePos.x -= movementSpeed;
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			playerOnePos.x += movementSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.W))
+		{
+			playerOnePos.y += movementSpeed;
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			playerOnePos.y -= movementSpeed;
+		}
+
+		playerOne.GetComponent<Transform>().position = playerOnePos;
+		playerTwo.GetComponent<Transform>().position = playerTwoPos;
+
 	}
 }
